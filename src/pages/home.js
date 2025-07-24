1e2f4ea1ca5c2eb6626dc148331d9881585835aa
Rahul <rahul.wa47@gmail.com>
2025-07-24 21:54:11 +0530

    created image folder with images of the dishes and test to check if image is rendering on the page

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,11 +2,16 @@
 export const loadHome = function () {
     console.log("home clicked");
     const homeSection = document.createElement("div")
-    homeSection.classList.add(".home-section")
+    homeSection.classList.add("home-section")
 
     const title = document.createElement("h1");
-    title.textContent = "Home"
+    title.textContent = "Dosa Junction"
+    
+    const description = document.createElement("p")
+    description.textContent = "Welcome to Dosa Junction - your cozy stop for mouthwatering South Indian classics, served fresh and affordable. Enjoy warm dosa, idili and snacks at an affordable price!"
+
     homeSection.appendChild(title);
+    homeSection.appendChild(description)
     return homeSection
     
 }
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,12 +1,32 @@
+import dosaImg01 from '../images/dosa01.jpg'
+
 
 export const loadMenu = function () {
+    const menuItems = [
+        {
+            image:"../images/dosa01.jpg",
+            name: "Dosa",
+            price: "$3.99",
+            description: "Crispy dosa with variety of chutneys"
+        }
+
+    ]
+
     console.log("menu clicked");
     const menuSection = document.createElement("div")
-    menuSection.classList.add(".menu-section")
+    menuSection.classList.add("menu-section")
+
+    const menuGrid = document.createElement("div")
+    menuGrid.classList.add("grid-container")
+
+    const img = document.createElement('img');
+    img.src = dosaImg01;
+    menuGrid.appendChild(img);
 
     const title = document.createElement("h1");
     title.textContent = "Menu"
     menuSection.appendChild(title);
+    menuSection.appendChild(menuGrid)
     return menuSection
     
 }
