695052b2fba9d7491a062b17769174ac07fa91b1
Rahul <rahul.wa47@gmail.com>
2025-07-26 12:39:39 +0530

    added active state to nav tabs, also added layout styling to home page

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
-import "./styles.css"
-import { loadHome } from "./pages/home";
-import { loadMenu } from "./pages/menu";
-import { loadAbout } from "./pages/about";
-import { initialLoad } from "./initialPageLoad";
+import './styles.css'
+import { loadHome } from './pages/home'
+import { loadMenu } from './pages/menu'
+import { loadAbout } from './pages/about'
+import { initialLoad } from './initialPageLoad'
 
-
-const contentSection = document.querySelector("#content");
+const contentSection = document.querySelector('#content')
 initialLoad()
 
-document.querySelectorAll(".nav-btn").forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        console.log(btn.id);
-        renderSection(btn.id)
-    })
+document.querySelectorAll('.nav-btn').forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    console.log(btn.id)
+    resetActiveClass()
+    renderSection(btn.id)
+    btn.classList.add('active-btn')
+  })
 })
 
+const resetActiveClass = function () {
+  document.querySelectorAll('.nav-btn').forEach((btn) => {
+    btn.classList.remove('active-btn')
+  })
+}
+
 const renderSection = function (section) {
-    console.log(section);
-    contentSection.innerHTML = ""
-    if(section === "home") {
-        contentSection.appendChild(loadHome())
-    }else if (section === "menu") {
-        contentSection.appendChild(loadMenu())
-    }else if (section === "about") {
-        contentSection.appendChild(loadAbout())
-    }   
+  console.log(section)
+  contentSection.innerHTML = ''
+  if (section === 'home') {
+    contentSection.appendChild(loadHome())
+  } else if (section === 'menu') {
+    contentSection.appendChild(loadMenu())
+  } else if (section === 'about') {
+    contentSection.appendChild(loadAbout())
+  }
 }
