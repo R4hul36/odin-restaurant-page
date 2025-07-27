80a771b2877ff30e876761947e326f776c5c4895
Rahul <rahul.wa47@gmail.com>
2025-07-27 17:28:18 +0530

    added content and styling to the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,17 @@ export const loadAbout = function () {
     aboutSection.classList.add("about-section")
 
     const title = document.createElement("h1");
-    title.textContent = "About"
+    title.textContent = "About Us"
+
+    const introPara = document.createElement('p')
+    introPara.textContent = "At Dosa Junction, we celebrate the rich culinary heritage of South India. From crispy dosas to flavorful chutneys, every dish is crafted with authentic ingredients and love."
+
+
+    const secondPara = document.createElement('p')
+    secondPara.textContent = "Our mission is to serve delicious, freshly made South Indian food that reminds you of home. We believe great taste shouldn't come at a high price."
+
     aboutSection.appendChild(title);
+    aboutSection.appendChild(introPara)
+    aboutSection.appendChild(secondPara)
     return aboutSection
 }
